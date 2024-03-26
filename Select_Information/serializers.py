8469da4a9009c0caba18662f109af15ea82d18c0
Joe-0620 <110929909+Joe-0620@users.noreply.github.com>
2024-03-26 13:39:50 +0800

    update StudentProfessorChoiceSerializer

--- Select_Information/serializers.py
+++ Select_Information/serializers.py
@@ -13,14 +13,21 @@ class StudentProfessorChoiceSerializer(serializers.ModelSerializer):
     professor_id = serializers.CharField(source='professor.id')
     professor = serializers.CharField(source='professor.name')
     professor_department = serializers.CharField(source='professor.department')
+    student_avatar = serializers.CharField(source='student.avatar')
     student_phone = serializers.CharField(source='student.phone_number')
+    student_initial_exam_score = serializers.CharField(source='student.initial_exam_score')
+    student_secondary_exam_score = serializers.CharField(source='student.secondary_exam_score')
+    student_initial_rank = serializers.CharField(source='student.initial_rank')
+    student_secondary_rank = serializers.CharField(source='student.secondary_rank')
+    student_final_rank = serializers.CharField(source='student.final_rank')
 
     class Meta:
         model = StudentProfessorChoice
         # fields = '__all__'  # 或者指定您想要序列化的字段
         fields = ['student', 'student_subject', 'student_type', 'student_postgraduate_type', 'student_id', 
                   'professor', 'professor_id', 'professor_department', 'status', 'chosen_by_professor', 'submit_date', 
-                  'finish_time', 'student_phone']
+                  'finish_time', 'student_phone', 'student_avatar', 'student_initial_exam_score', 'student_secondary_exam_score',
+                  'student_initial_rank', 'student_secondary_rank', 'student_final_rank']
 
     def get_student_name(self, obj):
         return obj.student.name
